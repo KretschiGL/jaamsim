72b9c4437c99a1c08c3ad9e4e47145ba108c8c75
Lukas Kretschmar <lukas.kretschmar@hsr.ch>
2020-04-30 11:46:02 +0200

    Added constants

---
+++ src/main/java/ch/hsr/plm/jaamsim/Transportation/Traveling.java
@@ -0,0 +1,10 @@
+package ch.hsr.plm.jaamsim.Transportation;
+
+public class Traveling {
+    public static final double LIGHT_SPEED = 299792458;
+
+    public static final double MIN_SPEED = 0.001d;
+    public static final double DEFAULT_SPEED = 1.0d;
+    public static final double MAX_SPEED = LIGHT_SPEED;
+
+}
